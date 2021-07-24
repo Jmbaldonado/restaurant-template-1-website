b1d351aeac5d2c350fccd026c4a50bf5d5b496d7
John Michael <84777760+Jmbaldonado@users.noreply.github.com>
2021-07-24 19:04:12 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,41 @@
+var nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function() {
+    if(window.pageYOffset > 100) {
+        nav.classList.add('bg-dark', 'shadow');
+    } else {
+        nav.classList.remove('bg-dark', 'shadow');
+    }
+})
+
+const btnscrolltoTop = document.querySelector('#btnscrolltoTop');
+
+window.addEventListener('scroll', scrollFunction);
+
+function scrollFunction(){
+
+    if(window.pageYOffset > 300){
+
+        if(!btnscrolltoTop.classList.contains('btnFade-in')){
+            btnscrolltoTop.classList.remove('btnFade-out');
+            btnscrolltoTop.classList.add('btnFade-in');
+            btnscrolltoTop.style.display = 'block';
+        }
+    } else {
+
+        if(btnscrolltoTop.classList.contains('btnFade-in')){
+            btnscrolltoTop.classList.remove('btnFade-in');
+            btnscrolltoTop.classList.add('btnFade-out');
+
+            setTimeout(function(){
+                btnscrolltoTop.style.display = 'none';
+            }, 250);
+        }
+    }
+}
+
+btnscrolltoTop.addEventListener('click', backtoTop);
+
+function backtoTop() {
+    window.scrollTo(0, 0);
+}
